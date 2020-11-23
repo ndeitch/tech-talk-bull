047c28c75c67290862e0e4f2b2f7aaf3efe118a6
Natan Deitch <natan.luz.deitch@gmail.com>
2020-11-23 14:04:14 -0300

    feat: bull board

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common'
 import { HelloController } from './controller/hello.controller'
 import { JobController } from './controller/job.controller'
 import { SimpleProcessor } from './processor/simple.processor'
+import { QueueModule } from './queue.module'
 
 @Module({
-  imports: [BullModule.registerQueue({ name: 'simple' })],
+  imports: [BullModule.registerQueue({ name: 'simple' }), QueueModule.forRoot({ name: 'simple' })],
   controllers: [HelloController, JobController],
   providers: [SimpleProcessor],
 })
